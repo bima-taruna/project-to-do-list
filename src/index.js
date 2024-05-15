948f7f26ad98652269b4adaaaf25fef36c90849a
bima-taruna <bimataruna21@gmail.com>
2024-05-15 10:23:17 +0700

    style basic layout

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import "./style.css";
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
+
+function init() {
+  const content = document.getElementById("content");
+  content.innerHTML = `<h1>CONTENT</h1>`;
+}
+
+init();
