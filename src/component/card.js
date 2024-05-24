3a83288fbaf85c356234798bdf0883c43e8c3792
bima-taruna <bimataruna21@gmail.com>
2024-05-24 22:38:38 +0700

    <improve>[allProjectDOM]: styling the buttons in project card

--- src/component/card.js
+++ src/component/card.js
@@ -9,17 +9,17 @@ class Card {
   createCardBody(className) {
     const sanitizedName = DOMPurify.sanitize(this.name);
     const buttonDiv = document.createElement("div");
-    buttonDiv.classList.add("button-div");
     const deleteButton = document.createElement("button");
     const editButton = document.createElement("button");
+    buttonDiv.classList.add("button-div");
     editButton.classList.add(className + "-edit");
     editButton.classList.add("material-icons");
     editButton.textContent = "edit";
     deleteButton.classList.add(className + "-delete");
     deleteButton.classList.add("material-icons");
     deleteButton.textContent = "delete";
-    buttonDiv.appendChild(deleteButton);
     buttonDiv.appendChild(editButton);
+    buttonDiv.appendChild(deleteButton);
     this.#cardBody = document.createElement("div");
     this.#cardBody.classList.add(className);
     this.#cardBody.textContent = sanitizedName;
