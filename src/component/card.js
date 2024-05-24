ca9492ddee95ee8796d155a0dab7ec07b36580db
bima-taruna <bimataruna21@gmail.com>
2024-05-24 22:32:13 +0700

    <improve>[card]: add delete button and edit button to card

--- src/component/card.js
+++ src/component/card.js
@@ -8,9 +8,22 @@ class Card {
 
   createCardBody(className) {
     const sanitizedName = DOMPurify.sanitize(this.name);
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("button-div");
+    const deleteButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    editButton.classList.add(className + "-edit");
+    editButton.classList.add("material-icons");
+    editButton.textContent = "edit";
+    deleteButton.classList.add(className + "-delete");
+    deleteButton.classList.add("material-icons");
+    deleteButton.textContent = "delete";
+    buttonDiv.appendChild(deleteButton);
+    buttonDiv.appendChild(editButton);
     this.#cardBody = document.createElement("div");
     this.#cardBody.classList.add(className);
     this.#cardBody.textContent = sanitizedName;
+    this.#cardBody.appendChild(buttonDiv);
     return this.#cardBody;
   }
 }
