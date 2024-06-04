70de45244b10a15c95dd3ebe8102cc41eb1eb7a4
bima-taruna <bimataruna21@gmail.com>
2024-06-04 16:05:07 +0700

    <feature>[projectDetail]: create Project Detail class and its constructor

---
+++ src/projectDetail.js
@@ -0,0 +1,8 @@
+class ProjectDetail {
+  constructor() {
+    const detailContainer = document.createElement("div");
+    detailContainer.classList.add("detail-container");
+  }
+}
+
+export const projectDetail = new ProjectDetail();
