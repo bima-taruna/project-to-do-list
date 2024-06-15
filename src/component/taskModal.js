7269c3bd8bdef9373edcd059326662704ebb8a2f
bima-taruna <bimataruna21@gmail.com>
2024-06-15 10:51:15 +0700

    <improve>[taskModal]: add date input and select to task form

--- src/component/taskModal.js
+++ src/component/taskModal.js
@@ -9,6 +9,14 @@ class TaskModal extends Modal {
                 <input type="text" id="task-name" name="task-name" required minlength="4" maxlength="20" required/>
                 <label for="task-desc">Description : </label>
                 <textarea id="task-desc" name="task-desc" rows="5" placeholder="Enter your project desc here...."></textarea>
+                <label for="task-date">Due Date : </label>
+                <input type="date" id="task-date" name="task-date"/>
+                <label for="task-priority">Priority : </label>
+                <select name="task-priority" id="task-priority">
+                  <option value="low">Low</option>
+                  <option value="medium">Medium</option>
+                  <option value="high">High</option>
+                </select>
                 <button class="task-add"></button>
             </form>
         `;
