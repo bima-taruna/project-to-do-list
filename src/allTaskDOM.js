cbca375b16df2d2a46f81d83c0290fd123fea7b9
bima-taruna <bimataruna21@gmail.com>
2024-07-02 17:41:26 +0700

    <improve>[allTaskDOM]:create fetchTask method

--- src/allTaskDOM.js
+++ src/allTaskDOM.js
@@ -9,4 +9,26 @@ class AllTaskDOM {
   get allTask() {
     return this.allTask;
   }
+
+  render(tasks) {
+    this.fetchTask(tasks);
+  }
+
+  fetchTask(tasks) {
+    this.allTask.textContent = "Tasks";
+    if (tasks && tasks.length > 0) {
+      while (this.allTask.children.length > 0) {
+        this.allTask.childNodes.forEach((item) => {
+          this.allTask.removeChild(item);
+        });
+      }
+      const taskName = tasks.map((task) => task.name);
+      taskName.forEach((item) => {
+        let newtaskCard = document.createElement("div");
+        this.allTask.appendChild(newtaskCard);
+      });
+    } else {
+      this.allTask.textContent = "empty..";
+    }
+  }
 }
