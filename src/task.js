cd2a1a2ac200cd790554cc8f9439e9debff497bd
bima-taruna <bimataruna21@gmail.com>
2024-07-15 21:49:00 +0700

    <improve>[task]: add id property using uuid

--- src/task.js
+++ src/task.js
@@ -1,5 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
+
 class Task {
   constructor(title, desc, date, priority, isFinish = false) {
+    this.id = uuidv4();
     this.title = title;
     this.desc = desc;
     this.date = date;
