3315dedc5c2409d3e06dc31b5a0d42e45ed91747
bima-taruna <bimataruna21@gmail.com>
2024-07-15 15:44:30 +0700

    <refact>[task]:refactoring task classes

--- src/task.js
+++ src/task.js
@@ -1,55 +1,10 @@
 class Task {
-  title;
-  desc;
-  date;
-  priority;
-  isFinish;
   constructor(title, desc, date, priority, isFinish = false) {
     this.title = title;
     this.desc = desc;
     this.date = date;
     this.priority = priority;
     this.isFinish = isFinish;
-  }
-
-  get title() {
-    return this.title;
-  }
-
-  set title(newTitle) {
-    this.title = newTitle;
-  }
-
-  get desc() {
-    return desc;
-  }
-
-  set desc(newDesc) {
-    this.desc = newDesc;
-  }
-
-  get date() {
-    return this.date;
-  }
-
-  set date(newDate) {
-    this.date = newDate;
-  }
-
-  get priority() {
-    return this.priority;
-  }
-
-  set priority(newPriority) {
-    this.priority = newPriority;
-  }
-
-  get isFinish() {
-    return this.isFinish;
-  }
-
-  set isFinish(newState) {
-    this.isFinish = newState;
   }
 }
 
