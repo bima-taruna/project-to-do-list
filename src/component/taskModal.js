a17761fc51b4a9cb72676619810a417c39f542f2
bima-taruna <bimataruna21@gmail.com>
2024-07-19 14:28:33 +0700

    <refact>[taskModal]: restyling the task modal

--- src/component/taskModal.js
+++ src/component/taskModal.js
@@ -19,14 +19,20 @@ class TaskModal extends Modal {
                 <input type="text" id="task-name" name="task-name" required minlength="4" maxlength="20" required/>
                 <label for="task-desc">Description : </label>
                 <textarea id="task-desc" name="task-desc" rows="5" placeholder="Enter your project desc here...."></textarea>
-                <label for="task-date">Due Date : </label>
-                <input type="date" id="task-date" name="task-date"/>
-                <label for="task-priority">Priority : </label>
-                <select name="task-priority" id="task-priority">
-                  <option value="low">Low</option>
-                  <option value="medium">Medium</option>
-                  <option value="high">High</option>
-                </select>
+                <div class="second-half">
+                  <div> 
+                    <label class="label-date" for="task-date">Due Date : </label>
+                    <input type="date" id="task-date" name="task-date"/>
+                  </div>
+                  <div>
+                    <label class="label-priority" for="task-priority">Priority : </label>
+                    <select name="task-priority" id="task-priority">
+                      <option value="low">Low</option>
+                      <option value="medium">Medium</option>
+                      <option value="high">High</option>
+                    </select>
+                  </div>
+                </div>
                 <button class="task-add"></button>
             </form>
         `;
