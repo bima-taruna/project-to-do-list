49bd3ccfccd401216256cf8a459fd13fb0336679
bima-taruna <bimataruna21@gmail.com>
2024-07-20 11:40:22 +0700

    <fix>[distanceToNow]: fix issue where the month keep on january

--- src/helper/distanceToNow.js
+++ src/helper/distanceToNow.js
@@ -1,11 +1,14 @@
 import { formatDistanceToNow, parse } from "date-fns";
 
 export function distanceDateToNow(params) {
-  const parsedDate = parse(params, "yyyy-mm-dd", new Date());
+  const parsedDate = parse(params, "yyyy-MM-dd", new Date());
+  const resultString = formatDistanceToNow(parsedDate, {
+    addSuffix: true,
+  });
   const isNow =
     new Date().toISOString().split("T")[0] === params ? true : false;
   return {
-    resultString: formatDistanceToNow(parsedDate, { addSuffix: true }),
+    resultString: resultString,
     isNow: isNow,
   };
 }
