02b284c22c4a488a91ebc552cad13fa1a1939a83
bima-taruna <bimataruna21@gmail.com>
2024-07-21 17:17:50 +0700

    <improve>[distanceToNow]: add check if the task in the past

--- src/helper/distanceToNow.js
+++ src/helper/distanceToNow.js
@@ -1,14 +1,17 @@
 import { formatDistanceToNow, parse } from "date-fns";
+import { isPast } from "date-fns/isPast";
 
 export function distanceDateToNow(params) {
   const parsedDate = parse(params, "yyyy-MM-dd", new Date());
   const resultString = formatDistanceToNow(parsedDate, {
     addSuffix: true,
   });
-  const isNow =
-    new Date().toISOString().split("T")[0] === params ? true : false;
+  const dateToString = new Date().toISOString().split("T")[0];
+  const isNow = dateToString === params ? true : false;
+  const isDatePast = isPast(parsedDate);
   return {
     resultString: resultString,
     isNow: isNow,
+    isPast: isDatePast,
   };
 }
